270b0de79598a59d4ae5fc0e7e18bb74f0b9db5f
George <gosha6216@gmail.com>
2026-03-01 10:38:50 -0800

    Version 1.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import './styles'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/app";
@@ -19,4 +7,3 @@ createRoot(document.getElementById("root")).render(
         <App />
     </StrictMode>,
 );
->>>>>>> release
